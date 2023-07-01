5328083cd1849f84945dadc67408f4e86d4e4b68
Chris Crane <chrisjcrane@gmail.com>
2023-07-01 09:19:18 -0600

    minor refactor

--- code.rb
+++ code.rb
@@ -2,18 +2,17 @@ require "date"
 require "pry"
 
 def pills_distribution(total_pills)
-  raise "Invalid total number of pills" unless total_pills.is_a?(Integer) && total_pills.positive?
+  raise "Total number of pills must be a positive number" unless total_pills.is_a?(Integer) && total_pills.positive?
   raise "Total number of pills must be even" unless total_pills.even?
 
+  puts "Total pills: #{total_pills}\n\n"
   
+  date = Date.today
+  pills_per_day = 2 # One in the morning and one at lunch
+
   chris = {pills: 0}
   sam = {pills: 0}
   april = {pills: 0}
-  
-  puts "Total pills: #{total_pills}\n"
-  
-  date = Date.today
-  pills_per_day = 2
 
   people = {
     monday: [sam, april],
@@ -28,7 +27,6 @@ def pills_distribution(total_pills)
   while total_pills > 0
     total_pills -= pills_per_day
     current_day = date.strftime("%A").downcase.to_sym
-    require 'pry'; binding.pry
     pills_per_person = pills_per_day / people[current_day].length
     people[current_day].each { |person| person[:pills] += pills_per_person }
     date += 1
