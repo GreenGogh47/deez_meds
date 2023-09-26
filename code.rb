895323e9582b60adb97a11da8158beed36794f00
Chris Crane <chrisjcrane@gmail.com>
2023-09-26 16:39:59 -0600

    add start day and end day indicators

--- code.rb
+++ code.rb
@@ -5,11 +5,13 @@ def pills_distribution(total_pills)
   raise "Total number of pills must be a positive number" unless total_pills.is_a?(Integer) && total_pills.positive?
   raise "Total number of pills must be even" unless total_pills.even?
 
-  puts "Total pills: #{total_pills}\n\n"
+  date = Date.today
+  pills_per_day = 2
   
-  date = Date.today + 6 # Start on Wednesday after Labor Day break
-  pills_per_day = 2 # One in the morning and one at lunch
-
+  puts "Total pills: #{total_pills}\n\n"
+  puts "Starting day: #{date}\n"
+  puts "Ending day: #{(date + (total_pills / pills_per_day))}\n"
+  
   chris = {pills: 0}
   sam = {pills: 0}
   school = {pills: 0}
@@ -38,13 +40,17 @@ def pills_distribution(total_pills)
     end
   end
 
-  # 4th of July I have them for lunch instead of school
-  # chris[:pills] += 1
-  # school[:pills] -= 1
+  # 13th no school
+  chris[:pills] += 1
+  school[:pills] -= 1
+
+  # 20th no school
+  chris[:pills] += 1
+  school[:pills] -= 1
 
   puts "Chris: #{chris[:pills]}"
   puts "Sam: #{sam[:pills]}"
   puts "School: #{school[:pills]}"
 end
 
-pills_distribution(28)
+pills_distribution(60)
